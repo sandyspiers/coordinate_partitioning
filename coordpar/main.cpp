38fddc6b7cc59d4e26dc6e5f036756ff49ba30b8
sandyspiers <sandy.spiers@gmail.com>
2023-09-19 12:47:02 +0000

    new experimental setup

--- coordpar/main.cpp
+++ coordpar/main.cpp
@@ -4,6 +4,7 @@
 
 #include "cut_plane.hpp"
 #include "diversity_problem.hpp"
+#include "glover.hpp"
 #include "partitioned_solver.hpp"
 #include "semaphore.hpp"
 
@@ -12,9 +13,9 @@ using std::vector;
 void run_test(string type, int n, int p, int s, int k,
               const vector<int> timelimits, const vector<string>& strategies,
               const vector<double>& partition_ratios, const string& filename,
-              Semaphore* sem) {
+              Semaphore* sem, bool glover) {
   // Create a DiversityProblem instance with random locations
-  DiversityProblem dp(type, n, p, s, k);
+  const DiversityProblem dp(type, n, p, s, k);
   bool low_mem = false;
   // Solve under each strategy
   for (const string& strategy : strategies) {
@@ -26,28 +27,31 @@ void run_test(string type, int n, int p, int s, int k,
     }
   }
   // Solve on cut-plane
-  CutPlaneSolver ct(dp, false);
+  CutPlaneSolver ct(dp, low_mem);
   ct.set_timelimit(timelimits);
   ct.solve();
   ct.write_results(filename);
+
+  // Solve on glover
+  if (glover) {
+    GloverSolver gv(dp);
+    gv.set_timelimit(timelimits);
+    gv.solve();
+    gv.write_results(filename);
+  }
 
   // Finish up
   sem->notify();
 }
 
-void random_box_test(const vector<int> timelimits,
-                     const vector<string>& strategies,
-                     const vector<double>& partition_ratios) {
-  // Problem settings
-  const vector<int> N = {500, 250, 100};
-  const vector<double> P_ratio = {0.1, 0.2};
-  const vector<int> S = {20, 15, 10, 5, 2};
-  const int K = 5;
-  // Output
-  const string filename = "res/random_box.txt";
-
+void run_parallel_test(int threads, const vector<int>& N,
+                       const vector<double>& P_ratio, const vector<int>& S,
+                       const int K, const vector<int>& timelimits,
+                       const vector<string>& strategies,
+                       const vector<double>& partition_ratios,
+                       const string filename, bool glover) {
   // Create a vector to store our futures objects
-  Semaphore sem(K * 2);
+  Semaphore sem(threads);
   vector<std::future<void>> futures;
   for (int n : N) {
     for (double p_ratio : P_ratio) {
@@ -55,9 +59,9 @@ void random_box_test(const vector<int> timelimits,
       for (int s : S) {
         for (int k = 0; k < K; k++) {
           sem.wait();
-          futures.push_back(std::async(std::launch::async, run_test, "random",
-                                       n, p, s, k, timelimits, strategies,
-                                       partition_ratios, filename, &sem));
+          futures.push_back(std::async(
+              std::launch::async, run_test, "random", n, p, s, k, timelimits,
+              strategies, partition_ratios, filename, &sem, glover));
         }
       }
     }
@@ -68,9 +72,32 @@ void random_box_test(const vector<int> timelimits,
   }
 }
 
-void random_box_test_large(const vector<int> timelimits,
-                           const vector<string>& strategies,
-                           const vector<double>& partition_ratios) {
+void random_box_test_small() {
+  // Solver settings
+  const vector<int> timelimits = {30, 60, 120, 300, 600};
+  const vector<string> strategies = {"random", "stratified", "greedy",
+                                     "stepped"};
+  const vector<double> partition_ratios = {0.75, 0.5, 0.25, 0.1};
+  const bool glover = false;
+  // Problem settings
+  const vector<int> N = {500, 250, 100};
+  const vector<double> P_ratio = {0.1, 0.2};
+  const vector<int> S = {20, 15, 10, 5, 2};
+  const int K = 5;
+  // Output
+  const string filename = "res/random_box_small.txt";
+  // Run parallel, using all cores
+  run_parallel_test(16, N, P_ratio, S, K, timelimits, strategies,
+                    partition_ratios, filename, glover);
+}
+
+void random_box_test_large() {
+  // Solver settings
+  const vector<int> timelimits = {30, 60, 120, 300, 600};
+  const vector<string> strategies = {"random", "stratified", "greedy",
+                                     "stepped"};
+  const vector<double> partition_ratios = {0.75, 0.5, 0.25, 0.1};
+  const bool glover = false;
   // Problem settings
   const vector<int> N = {1000};
   const vector<double> P_ratio = {0.1, 0.2};
@@ -78,32 +105,17 @@ void random_box_test_large(const vector<int> timelimits,
   const int K = 5;
   // Output
   const string filename = "res/random_box_large.txt";
-
-  // Create a vector to store our futures objects
-  Semaphore sem(8);
-  vector<std::future<void>> futures;
-  for (int n : N) {
-    for (double p_ratio : P_ratio) {
-      int p = n * p_ratio;
-      for (int s : S) {
-        for (int k = 0; k < K; k++) {
-          sem.wait();
-          futures.push_back(std::async(std::launch::async, run_test, "random",
-                                       n, p, s, k, timelimits, strategies,
-                                       partition_ratios, filename, &sem));
-        }
-      }
-    }
-  }
-  // Wait for all threads to complete
-  for (auto& future : futures) {
-    future.wait();
-  }
+  // Run parallel, using half cores
+  run_parallel_test(8, N, P_ratio, S, K, timelimits, strategies,
+                    partition_ratios, filename, glover);
 }
 
-void random_circle_test(const vector<int> timelimits,
-                        const vector<string>& strategies,
-                        const vector<double>& partition_ratios) {
+void random_circle_test() {
+  // Solver settings
+  const vector<int> timelimits = {30, 60, 120, 300, 600};
+  const vector<string> strategies = {"random", "stratified"};
+  const vector<double> partition_ratios = {0.75, 0.5, 0.25};
+  const bool glover = true;
   // Problem settings
   const vector<int> N = {100, 50};
   const vector<double> P_ratio = {0.1, 0.2};
@@ -111,43 +123,14 @@ void random_circle_test(const vector<int> timelimits,
   const int K = 5;
   // Output
   const string filename = "res/random_circle.txt";
-
-  // Create a vector to store our futures objects
-  Semaphore sem(K * 2);
-  vector<std::future<void>> futures;
-  for (int n : N) {
-    for (double p_ratio : P_ratio) {
-      int p = n * p_ratio;
-      for (int s : S) {
-        for (int k = 0; k < K; k++) {
-          sem.wait();
-          futures.push_back(std::async(std::launch::async, run_test, "circle",
-                                       n, p, s, k, timelimits, strategies,
-                                       partition_ratios, filename, &sem));
-        }
-      }
-    }
-  }
-  // Wait for all threads to complete
-  for (auto& future : futures) {
-    future.wait();
-  }
+  // Run parallel, using all cores
+  run_parallel_test(16, N, P_ratio, S, K, timelimits, strategies,
+                    partition_ratios, filename, glover);
 }
 
 int main() {
-  // Solver settings
-  const vector<int> timelimits = {30, 60, 120, 300, 600};
-  const vector<string> strategies = {"random", "stratified", "greedy",
-                                     "stepped"};
-  const vector<double> partition_ratios = {0.75, 0.5, 0.25, 0.1};
-
-  random_box_test(timelimits, strategies, partition_ratios);
-  random_box_test_large(timelimits, strategies, partition_ratios);
-
-  // Circle is very difficult, nothing can solve, so just use the best possible
-  const vector<string> strategies_circle = {"random", "stratified"};
-  const vector<double> partition_ratios_circle = {0.75, 0.5, 0.25};
-  random_circle_test(timelimits, strategies_circle, partition_ratios_circle);
-
+  random_box_test_small();
+  random_box_test_large();
+  random_circle_test();
   return 0;
 }
